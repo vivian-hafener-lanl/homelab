07c831edba6de95fdb12cc12322c913264e8b4f7
Vivian Hafener <79525734+viv-codes@users.noreply.github.com>
2022-02-10 15:33:47 -0500

    Update dockersetup.sh

--- dockersetup.sh
+++ dockersetup.sh
@@ -4,7 +4,7 @@ apt update
 apt upgrade -y
 apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 apt update
 apt install docker-ce -y
 
