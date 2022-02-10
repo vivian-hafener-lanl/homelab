6e66e5e9bb5cbf14e632b31aa7c24331727b16c3
Vivian Hafener <79525734+viv-codes@users.noreply.github.com>
2022-02-10 14:57:10 -0500

    Update and rename rkesetup.sh to dockersetup.sh

--- dockersetup.sh
+++ dockersetup.sh
@@ -2,9 +2,7 @@
 
 apt update
 apt upgrade -y
-apt install apt-transport-https ca-certificates curl software-properties-common qemu-guest-agent -y
-systemctl enable qemu-guest-agent
-systemctl start qemu-guest-agent
+apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt update
